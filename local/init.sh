19c63448ac4d26cab26e859e4f1f1d84b428f7bb
Aaron Birkland <apb@jhu.edu>
2020-07-09 08:47:46 -0400

    Encode git and ISLE repos,branches as env vars

--- local/init.sh
+++ local/init.sh
@@ -1,4 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+source .env
+git clean -fdX
+ISLE_BRANCH=${ISLE_BRANCH:-development}
+ISLE_REPO=${ISLE_REPO:-}
+
+function checkout { # checkout $repo $branch $into_dir
+  local REPO=$1
+  local BRANCH=$2
+  local DIR=$3
+  if [ -d $DIR ]; then
+    (cd $DIR; git checkout $BRANCH || exit )
+  else
+    git clone --branch $BRANCH $REPO $DIR || exit
+  fi
+}
+
+checkout $ISLE_REPO $ISLE_BRANCH .isle
+checkout $DRUPAL_SITE_REPO $DRUPAL_SITE_BRANCH codebase
+
+cp -r .isle/* .
 make
 docker-compose up -d
 docker-compose exec drupal with-contenv bash -lc 'COMPOSER_MEMORY_LIMIT=-1 composer install'
